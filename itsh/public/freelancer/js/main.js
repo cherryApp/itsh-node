4c50d9e6a56c650e4430e7e1d08a8a8670505f9f
cherryApp <j.cserko@gmail.com>
2017-04-19 16:30:37 +0200

    Feat: daily work.

--- itsh/public/freelancer/js/main.js
+++ itsh/public/freelancer/js/main.js
@@ -3,11 +3,16 @@ var itsh = angular.module('itsh', []);
 
 // Products controller.
 itsh.controller('ProductController', ['$scope', '$http', function($scope, $http) {
-    $scope.search = 'Portfolio';
+    // $scope.search = 'Portfolio';
+
+    $scope.checkRow = function(key) {
+        return ["_id", "__v"].indexOf(key) == -1;
+    };
 
     $http.get('api/product')
         .then( function(serverResponse) {
             console.log(serverResponse.data);
+            $scope.tableData = serverResponse.data;
         }, function(err) {
             console.error(err);
         });
