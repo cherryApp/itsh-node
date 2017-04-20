a90410b28c87bf348e3f25440472fe86036201cd
cherryApp <j.cserko@gmail.com>
2017-04-20 12:04:21 +0200

    Feat: add CRUD.

--- itsh/public/freelancer/js/main.js
+++ itsh/public/freelancer/js/main.js
@@ -4,6 +4,13 @@ var itsh = angular.module('itsh', []);
 // Products controller.
 itsh.controller('ProductController', ['$scope', '$http', function($scope, $http) {
     $scope.search = {};
+    $scope.newRow = {};
+    $scope.cols = [
+        {key: 'name', label: 'Name'},
+        {key: 'manufacturer', label: 'Man.'},
+        {key: 'price', label: 'Price'},
+        {key: 'active', label: 'Active'}
+    ];
 
     $scope.checkRow = function(key) {
         return ["_id", "__v"].indexOf(key) == -1;
@@ -14,6 +21,31 @@ itsh.controller('ProductController', ['$scope', '$http', function($scope, $http)
         $http.post('api/product/' + row._id, row)
             .then( function(serverResponse) {
                 console.log(serverResponse.data);
+            }, function(err) {
+                console.error(err);
+            });
+    };
+
+    $scope.deleteRow = function(row) {
+        console.log(row);
+        $http.delete('api/product/' + row._id)
+            .then( function(serverResponse) {
+                var res = serverResponse.data;
+                if (res.ok == 1) {
+                    var index = $scope.tableData.indexOf(row);
+                    $scope.tableData.splice(index, 1);
+                }
+            }, function(err) {
+                console.error(err);
+            });
+    };
+
+    $scope.insertRow = function(row) {
+        $http.put('api/product', row)
+            .then( function(serverResponse) {
+                var newRow = serverResponse.data;
+                $scope.tableData.push(newRow);
+                $scope.newRow = {};
             }, function(err) {
                 console.error(err);
             });
--- itsh/routes/api.js
+++ itsh/routes/api.js
@@ -1,65 +1,100 @@
 var express = require('express');
-var router = express.Router(); 
+var router = express.Router();
 var mongoose = require('mongoose');
 
 // Models.
 const models = {};
 models.product = mongoose.model('Product', {
-  name: String,
-  price: Number,
-  manufacturer: String,
-  active: Boolean
+    name: String,
+    price: Number,
+    manufacturer: String,
+    active: Boolean
 });
 
 models.user = mongoose.model('User', {
-  name: String,
-  email: String,
-  phone: String,
-  address: String,
-  active: Boolean
+    name: String,
+    email: String,
+    phone: String,
+    address: String,
+    active: Boolean
 });
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.send('invalid request');
+    res.send('invalid request');
 });
 
 router.get('/:collection', function(req, res, next) {
-  let collection = req.params.collection;
-  if (models[collection]) {
-      models[collection].find( (err, data) => {
-        if (err) {
-            res.writeHead(501, 'Database Error.');
-            return res.send();
-        }
-        res.json(data);
-      });
-  } else {
-      res.writeHead(404, 'No Colleciton!');
-      return res.send();
-  }
+    let collection = req.params.collection;
+    if (models[collection]) {
+        models[collection].find((err, data) => {
+            if (err) {
+                res.writeHead(501, 'Database Error.');
+                return res.send();
+            }
+            res.json(data);
+        });
+    } else {
+        res.writeHead(404, 'No Colleciton!');
+        return res.send();
+    }
 });
 
 // Update document.
 router.post('/:collection/:id', (req, res, next) => {
-  let collection = req.params.collection;
-  let id = req.params.id;
+    let collection = req.params.collection;
+    let id = req.params.id;
 
-  if (models[collection]) {
-    models[collection].update(
-      {_id: id},
-      req.body,
-      {},
-      (err, data) => {
-        if (err) {
-          return res.json(err);
-        }
-        res.json(data);
-      });
-  } else {
-    res.writeHead(404, 'No Colleciton!');
-    return res.send();
-  }
+    if (models[collection]) {
+        models[collection].update({ _id: id },
+            req.body, {},
+            (err, data) => {
+                if (err) {
+                    return res.json(err);
+                }
+                res.json(data);
+            });
+    } else {
+        res.writeHead(404, 'No Colleciton!');
+        return res.send();
+    }
+});
+
+// Delete document.
+router.delete('/:collection/:id', (req, res, next) => {
+    let collection = req.params.collection;
+    let id = req.params.id;
+
+    if (models[collection]) {
+        models[collection].remove({ _id: id },
+            (err, data) => {
+                if (err) {
+                    return res.json(err);
+                }
+                res.json(data);
+            });
+    } else {
+        res.writeHead(404, 'No Colleciton!');
+        return res.send();
+    }
+});
+
+// Create document.
+router.put('/:collection', (req, res, next) => {
+    let collection = req.params.collection;
+
+    if (models[collection]) {
+        let entity = new models[collection](req.body);
+        entity.save((err, data) => {
+            if (err) {
+                return res.json(err);
+            }
+            res.json(data);
+        });
+    } else {
+        res.writeHead(404, 'No Colleciton!');
+        return res.send();
+    }
 });
 
 module.exports = router;
